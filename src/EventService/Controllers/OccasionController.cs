b2524e566b70b402198312b0705c03b633d0f2a4
Segway-svg <tukatuta@mail.ru>
2023-05-10 07:52:53 +0300

    test

---
+++ src/EventService/Controllers/OccasionController.cs
@@ -0,0 +1,21 @@
+﻿// using System;
+// using System.Threading.Tasks;
+// using LT.DigitalOffice.EventService.Business.Commands.Event.Interfaces;
+// using LT.DigitalOffice.EventService.Models.Dto.Requests.Event;
+// using LT.DigitalOffice.Kernel.Responses;
+// using Microsoft.AspNetCore.Mvc;
+//
+// namespace LT.DigitalOffice.EventService.Controllers;
+//
+// [Route("[controller]")]
+// [ApiController]
+// public class OccasionController : ControllerBase
+// {
+//   [HttpPost("create")]
+//   public async Task<OperationResultResponse<Guid?>> CreateAsync(
+//     [FromServices] ICreateEventCommand command,
+//     [FromBody] CreateEventRequest request)
+//   {
+//     return await command.ExecuteAsync(request);
+//   }
+// }
